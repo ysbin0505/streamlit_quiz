ec469efb6af08faab6fbdfcb264048f143b9dca7
ysbin0505 <chfhddl515@gmail.com>
2025-09-04 12:24:02 +0900

    main 단순화

--- dataly_manager/datalyManager.py
+++ dataly_manager/datalyManager.py
@@ -8,8 +8,9 @@ APP_DIR = os.path.dirname(__file__)
 if APP_DIR not in sys.path:
     sys.path.insert(0, APP_DIR)
 
-from dataly_manager.ui.table_to_excel_ui import render_table_to_excel
-from dataly_manager.ui.photo_to_excel_ui import render_photo_to_excel
+from ui.table_to_excel_ui import render_table_to_excel
+from ui.photo_to_excel_ui import render_photo_to_excel
+
 
 
 
